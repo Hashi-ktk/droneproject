6fe688a01f9bc076d4c940ea357df6f6b11030af
Muhammad Hashir Imran <87969566+Hashi-ktk@users.noreply.github.com>
2024-05-12 01:57:02 +0500

    Update drone_stream.py

--- drone_stream.py
+++ drone_stream.py
@@ -30,5 +30,5 @@ def connect_to_drone():
         me = tello.Tello()
         me.connect()
         me.streamon()
-    return redirect(url_for('main.profile'))  # Redirect to profile page after connecting to drone
+    return redirect(url_for('main.streaming'))  # Redirect to profile page after connecting to drone
 
