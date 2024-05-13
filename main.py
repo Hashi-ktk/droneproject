9ab3a3ca03f0dc6087ba9d81e5792044d1895880
Muhammad Hashir Imran <87969566+Hashi-ktk@users.noreply.github.com>
2024-05-13 20:04:51 +0500

    frontend changes

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-# @main.route('/connect_to_drone')
-# @login_required
-# def connect_to_drone():
-#     return redirect(url_for('main.streaming'))
-
 @main.route('/streaming')
 @login_required
 def streaming():
