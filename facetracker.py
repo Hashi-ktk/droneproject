9028cc9eb26ebe1117258bde4d7bf1a1fb8ed3a9
Muneeb <muneeb@gracetechnologiespk.com>
2024-05-15 15:44:23 +0500

    Face Tracking

--- facetracker.py
+++ facetracker.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, Response
+from flask import Blueprint, Response
 import cv2
 from djitellopy import Tello
 import numpy as np
@@ -18,9 +18,10 @@ pError_y = 0
 # face limit area
 faceLimitArea = [8000, 10000]
 
-# drone take off start value
+# drone state variables
 takeoff = False
 land = False
+stop_tracking = False
 
 # Initialize Tello
 def init_tello():
@@ -53,26 +54,16 @@ def face_detect(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_faces = frontal_face.detectMultiScale(img_gray, 1.2, 8)
 
-    # if there are more than 1 face on the given image
     face_list = []
     face_list_area = []
 
-    # detected faces coordinates and sizes
     for (x, y, w, h) in img_faces:
-        # draw a rectangle around faces
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # center x, y coordinates
         cx = x + w // 2
         cy = y + h // 2
-
-        # save detected face coordinated and its area
         face_list.append([cx, cy])
         face_list_area.append(w * h)
 
-    # get face that closer on the camera (max area)
-    # return given image and face_info (detected face coordinates and size)
-    # that closer the camera
     if len(face_list_area) != 0:
         i = face_list_area.index(max(face_list_area))
         return img, [face_list[i], face_list_area[i]]
@@ -90,86 +81,75 @@ def face_track(tello, face_info, w, h, pid, pError, pError_y):
     speed = pid[0] * error + pid[1] * (error - pError)
     speed = int(np.clip(speed, -60, 60))
 
-    error_y = h // (12 / 5) - y
+    error_y = h // 2 - y
     speed_y = pid[0] * error_y + pid[1] * (error_y - pError_y)
     speed_y = int(np.clip(speed_y, -60, 60))
 
-    # set speeds to moving
     if x != 0:
         tello.yaw_velocity = speed
     else:
-        speed = 0
-        tello.yaw_velocity = speed
-        error = 0
+        tello.yaw_velocity = 0
 
     if y != 0:
         tello.up_down_velocity = speed_y
     else:
-        speed_y = 0
-        tello.up_down_velocity = speed_y
-        error_y = 0
+        tello.up_down_velocity = 0
 
-    # Controlling the area size and moving forward, backward
-    if area > faceLimitArea[0] and area < faceLimitArea[1]:
+    if faceLimitArea[0] < area < faceLimitArea[1]:
         forw_backw = 0
     elif area > faceLimitArea[1]:
         forw_backw = -10
     elif area < faceLimitArea[0] and area > 100:
         forw_backw = 10
 
-    # send adjusted forward backward with adjusted speed
-    tello.send_rc_control(0, forw_backw, speed_y, speed)
+    tello.send_rc_control(0, forw_backw, tello.up_down_velocity, tello.yaw_velocity)
     return error, error_y
 
 # Route handler for video stream
 @face_tracking.route('/video_feed')
 def video_feed():
-    global takeoff, pError, pError_y, land
+    global takeoff, pError, pError_y, land, stop_tracking
+    stop_tracking = False
     tello = init_tello()
-    while True:
-        # take off tello
-        if takeoff == 0:
-            try:
-                tello.takeoff()
-                tello.move_up(80)
-                takeoff = True
-                land = True
-                time.sleep(2.2)
-            except:
-                pass
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+    
+    def generate_frames():
+        global takeoff, pError, pError_y, land, stop_tracking
+        while True:
+            if stop_tracking:
                 if land:
-                    try:
-                        tello.streamoff()
-                        tello.land()
-                    except:
-                        pass
+                    tello.streamoff()
+                    tello.land()
+                    land = False
                 break
 
-        # Stream on and get frame
-        img = get_frame(tello, w, h)
+            if not takeoff:
+                try:
+                    tello.takeoff()
+                    tello.move_up(80)
+                    takeoff = True
+                    land = True
+                    time.sleep(2.2)
+                except Exception as e:
+                    print(f"Error during takeoff: {e}")
 
-        # Detect face on the frame
-        img, face_info = face_detect(img)
+            img = get_frame(tello, w, h)
+            img, face_info = face_detect(img)
+            pError, pError_y = face_track(tello, face_info, w, h, pid, pError, pError_y)
 
-        # Track face smoothly
-        # Get current error for pid as pError (previous error)
-        pError, pError_y = face_track(tello, face_info, w, h, pid, pError, pError_y)
+            img = cv2.putText(img, str(tello.get_battery()), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 100, 250), 1, cv2.LINE_AA)
+            img = cv2.putText(img, f'pError: {pError}', (0, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 0), 1, cv2.LINE_AA)
+            img = cv2.putText(img, f'pError_y: {pError_y}', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 0), 1, cv2.LINE_AA)
+            img = cv2.putText(img, f'Area: {face_info[1]}', (0, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 0), 1, cv2.LINE_AA)
+            
+            ret, jpeg = cv2.imencode('.jpg', img)
+            frame = jpeg.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-        # Show frame
-        img = cv2.putText(img, str(tello.get_battery()), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 100, 250), 1,
-                          cv2.LINE_AA)
-        img = cv2.putText(img, str('pError:' + str(pError)), (0, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 0), 1,
-                          cv2.LINE_AA)
-        img = cv2.putText(img, str('pError_y:' + str(pError_y)), (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 0),
-                          1,
-                          cv2.LINE_AA)
-        img = cv2.putText(img, str('Area:' + str(face_info[1])), (0, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 0),
-                          1,
-                          cv2.LINE_AA)
-        ret, jpeg = cv2.imencode('.jpg', img)
-        frame = jpeg.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@face_tracking.route('/stop_tracking')
+def stop_tracking_route():
+    global stop_tracking
+    stop_tracking = True
+    return "Face tracking stopped"
